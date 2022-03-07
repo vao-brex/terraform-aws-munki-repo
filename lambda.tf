522b1552b29ac3c5a92434a3b6aa4f3f52a0b1f7
vao-brex <vao@brex.com>
2022-03-07 15:12:49 -0800

    templatefile

--- lambda.tf
+++ lambda.tf
@@ -21,20 +21,17 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-data "template_file" "basic_auth_js" {
-  template = file("${path.module}/basic_auth.js.tpl")
-
-  vars = {
-    username = var.username
-    password = var.password
-  }
-}
-
 data "archive_file" "basic_auth_lambda_zip" {
   type = "zip"
 
-  output_path             = "basic_auth_lambda.zip"
-  source_content          = data.template_file.basic_auth_js.rendered
+  output_path = "basic_auth_lambda.zip"
+  source_content = templatefile(
+    "${path.module}/basic_auth.js.tpl",
+    {
+      username = var.username
+      password = var.password
+    }
+  )
   source_content_filename = "basic_auth.js"
 }
 
